57925a0b1ea075b3221e742cd9f6e3c3efcd0865
Franco <fsanchez.fns@gmail.com>
2020-05-07 01:13:51 -0300

    Se agrega redis, se debe encapsular la logica de conecction

--- app.js
+++ app.js
@@ -4,6 +4,26 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
+
+//conection con redis
+var redis = require('redis')
+const host = "192.168.99.100" //url que corre mi docker
+const port = 6379
+    //Connecting redis client
+const client = redis.createClient(port, host, redis)
+
+client.on('connect', function() {
+    console.log("Redis Connected")
+});
+client.on('error', function(err) {
+    console.log(err)
+});
+
+client.set('string key', 'franquito se conecto vos podes', redis.print);
+console.log(client.get('string key', redis.print))
+
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var clientsRouter = require('./routes/clients');
