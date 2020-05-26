2a5115f7c3374647fef58ba6d3d579a868942b00
Franco <fsanchez.fns@gmail.com>
2020-05-26 01:04:22 -0300

    se agregan variables de entorno

--- db/dbRedisConfig.js
+++ db/dbRedisConfig.js
@@ -1,8 +1,11 @@
+var env = require('node-env-file');
+env('./.env');
+
+const host = process.env.HOST_REDIS; //"192.168.99.100" //url que corre mi docker
+const port = process.env.PORT_REDIS;
+
 //conection con redis
 var redis = require('redis')
-const host = "192.168.99.100" //url que corre mi docker
-const port = 6379
-    //Connecting redis client
 const redisClient = redis.createClient(port, host, redis)
 
 redisClient.on('connect', function() {
